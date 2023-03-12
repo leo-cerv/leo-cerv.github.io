0cd4ecd11c2e47c8e5a9b9e6caf39fac22372700
leo-cerv <lscs.consultas@gmail.com>
2023-03-12 14:21:23 -0300

    feat: add global colors

--- src/components/Navbar/Navbar.styles.js
+++ src/components/Navbar/Navbar.styles.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
+import { colors } from "../../styles/colors";
 
 export const ContainerNavbar = styled.div`
 height: 100px;
-background-color: red;
+background-color: ${colors.TEXT_BLACK};
 
 `;
