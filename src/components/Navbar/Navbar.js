86fdbef37ceb346e67dbbf16cf9d9fbe99c87ec0
leo-cerv <lscs.consultas@gmail.com>
2023-03-12 15:35:05 -0300

    feat: add burguermenu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,41 @@
+import { useState } from 'react';
 import React from 'react'
 import * as S from "./Navbar.styles"
 
 export const Navbar = () => {
+
+const [showMenuBurguer, setShowMenuBurguer] = useState(false);
+
+
   return (
 
     // importo el nombre de la constante en syle componentent
     <S.ContainerNavbar>
-        Mi barra de navegador
+       <S.ContainerLogo>
+        <S.ImageLogo src="/img/logo.png" alt="Logo Leo C" />
+        <div>
+          <h1>Leo C</h1>
+          <h2>Developer</h2>
+
+        </div>
+
+       </S.ContainerLogo>
+
+        <S.ContainerBurguer onClick={() => setShowMenuBurguer(!showMenuBurguer)}>
+          <S.BurguerMenu></S.BurguerMenu>
+          <S.BurguerMenu></S.BurguerMenu>
+          <S.BurguerMenu></S.BurguerMenu>
+
+        </S.ContainerBurguer>
+
+
+
+       <S.ContainerItems showMenuBurguer={showMenuBurguer}>
+          <p>About me</p>
+          <p>Projects</p>
+          <S.Button>Contact me</S.Button>
+          
+       </S.ContainerItems>
 
     </S.ContainerNavbar>
   )
